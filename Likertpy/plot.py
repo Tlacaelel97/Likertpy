91a7d725221c2b96051f6cbf931345b70b58727a
Tlacaelel <tlacafv@gmail.com>
2024-11-26 11:51:05 -0600

    create Figure class ready

--- Likertpy/plot.py
+++ Likertpy/plot.py
@@ -1,40 +1,54 @@
 import plotly.graph_objects as go
-import pandas as pd
-
-fig = go.Figure()
-
-# Fake data
-df = pd.DataFrame()
-
-names = ["SF Zoo", "LA Zoo"]
-
-d1 = [20, 12]
-d2 = [14, 18]
-d3 = [23, 29]
-
-df["Zoo"] = names
-df["Giraffes"] = d1
-df["Orangutans"] = d2
-df["Monkeys"] = d3
-
-print(list(df.loc[df["Zoo"] == "SF Zoo"][list(df.columns[1:])]
-                .transpose()
-                .iloc[:, 0]))
-# print(df)
-for category in df["Zoo"].values:
-    fig.add_trace(
-        go.Bar(
-            y=df.columns[1:],
-            x=list(
-                df.loc[df["Zoo"] == category][list(df.columns[1:])]
-                .transpose()
-                .iloc[:, 0]
-            ),
-            name=str(category),
-            orientation="h"
-        )
-    )
 
 
-fig.update_layout(barmode="stack")
-fig.show()
+class createFigure:
+    """
+    This class is used to create a plot using plotly library.
+
+    Attributes:
+        None
+
+    Returns:
+        fig (go.Figure) -- Figure object to plot
+    """
+
+    def __init__(self):
+        pass
+
+    def createFig(self) -> go.Figure:
+        fig = go.Figure()
+        return fig
+
+
+class createPlot:
+    """
+
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
+    def __init__(self, df):
+        self.df = df
+
+    def likert_plot(self):
+        fig = createFigure().createFig()
+        for category in self.df["Zoo"].values:
+            fig.add_trace(
+                go.Bar(
+                    y=self.df.columns[1:],
+                    x=list(
+                        self.df.loc[self.df["Zoo"] == category][
+                            list(self.df.columns[1:])
+                        ]
+                        .transpose()
+                        .iloc[:, 0]
+                    ),
+                    name=str(category),
+                    orientation="h",
+                )
+            )
+
+        fig.update_layout(barmode="stack")
+        fig.show()
