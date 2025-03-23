d6bc9e2df1ebe5a7c8ed1e9098bf23e5220e19a2
Tlacaelel <tlacafv@gmail.com>
2025-03-23 13:14:26 -0600

    Max plot ready

--- Likertpy/plot.py
+++ Likertpy/plot.py
@@ -479,7 +479,7 @@ def plot_mode(
 
 
 def plot_max(
-    df: typing.Union[pd.DataFrame, pd.Series], group: str, **kwargs
+    df: typing.Union[str,pd.DataFrame, pd.Series], group: str, **kwargs
 ) -> matplotlib.axes.Axes:
     """
     Plots a heatmap of the maximum values across three cleaned DataFrames.
@@ -508,15 +508,18 @@ def plot_max(
         >>> plot_max(df, group="G1")
     """
     # Validate input types
-    if not isinstance(df, (pd.DataFrame, pd.Series)):
-        raise ValueError("The 'df' argument must be a pandas DataFrame or Series.")
-    if df.empty:
-        raise ValueError("The provided dataset is empty. Cannot compute mode.")
+    if not isinstance(df, (str, pd.DataFrame, pd.Series)):
+        raise HeatMapError("The 'df' argument must be a pandas DataFrame or Series.")
+    if isinstance(df, (pd.DataFrame, pd.Series)):
+        if df.empty:
+            raise HeatMapError("The provided dataset is empty. Cannot compute mode.")
     if not isinstance(group, str):
         raise TypeError("The 'group' argument must be a string.")
 
+    if isinstance(df, str):
+        data = FileRead(folder="IN", file=df).read_file_to_dataframe()
     # Clean and parse data
-    heathmap_data = _configure_data_for_heatmap(df, group)
+    heathmap_data = _configure_data_for_heatmap(fileName=df, data=data, group=group)
 
     # calculate maximum
     max_df = calculate_max(heathmap_data)
@@ -525,7 +528,7 @@ def plot_max(
     max_df = max_df.astype(float)
 
     # Create the heatmap
-    axes = _create_heatmap(max_df, "Máximo", group)
+    axes = _create_heatmap(max_df, "Máximo", group, fileName=df)
     return axes
 
 
@@ -601,142 +604,6 @@ def plot_gradient(
     axes = _create_heatmap(gradient_df, "Gradiente", group)
     return axes
 
-
-def calculate_max(
-    data: tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame],
-) -> pd.DataFrame:
-    """
-    Calculates the element-wise maximum across three DataFrames.
-
-    This function computes the maximum value for each corresponding cell position
-    across three given DataFrames. The result is a DataFrame where each cell contains
-    the maximum value of the corresponding cells from the three input DataFrames.
-
-    Args:
-        data (tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]): A tuple containing three
-            DataFrames with identical structures (indices and columns).
-
-    Returns:
-        pd.DataFrame: A DataFrame with the same structure as the input DataFrames, where
-        each cell contains the maximum value of the corresponding cells from the three DataFrames.
-
-    Example:
-        >>> df1 = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
-        >>> df2 = pd.DataFrame({"A": [3, 2, 1], "B": [6, 5, 4]})
-        >>> df3 = pd.DataFrame({"A": [2, 4, 3], "B": [5, 6, 7]})
-        >>> calculate_max((df1, df2, df3))
-           A  B
-        0  3  6
-        1  4  6
-        2  3  7
-    """
-    if len(data) != 3:
-        raise ValueError("The 'data' tuple must contain exactly three DataFrames.")
-    max_data = pd.DataFrame(
-        np.maximum(np.maximum(data[0], data[1]), data[2]), columns=data[2].columns
-    )
-    return max_data
-
-
-def calculate_min(
-    data: tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame],
-) -> pd.DataFrame:
-    """
-    Computes the element-wise minimum across three DataFrames.
-
-    This function takes a tuple containing three pandas DataFrames and returns a new
-    DataFrame where each cell contains the minimum value from the corresponding
-    cells of the three input DataFrames.
-
-    Args:
-        data (tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]): A tuple of three
-            DataFrames with identical structure (same indices and columns).
-
-    Returns:
-        pd.DataFrame: A DataFrame containing the element-wise minimum values.
-
-    Raises:
-        ValueError: If the input tuple does not contain exactly three DataFrames.
-        KeyError: If the DataFrames have misaligned indices or columns.
-
-    Example:
-        >>> df1 = pd.DataFrame({"A": [1, 5, 3], "B": [4, 2, 6]})
-        >>> df2 = pd.DataFrame({"A": [2, 3, 1], "B": [5, 1, 7]})
-        >>> df3 = pd.DataFrame({"A": [3, 4, 2], "B": [6, 0, 8]})
-        >>> calculate_min((df1, df2, df3))
-           A  B
-        0  1  4
-        1  3  0
-        2  1  6
-    """
-    if len(data) != 3:
-        raise ValueError("The 'data' tuple must contain exactly three DataFrames.")
-
-    try:
-        min_data = pd.DataFrame(
-            np.minimum(np.minimum(data[0], data[1]), data[2]), columns=data[2].columns
-        )
-    except KeyError as e:
-        raise KeyError("Mismatched indices or columns in input DataFrames.") from e
-
-    return min_data
-
-
-def calculate_gradient(
-    data: tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame],
-) -> pd.DataFrame:
-    """
-    Computes the gradient of change between successive DataFrames for Likert-scale data.
-
-    This function calculates the element-wise gradient of change between three
-    successive DataFrames, where the data represents responses on a Likert scale
-    (values ranging from 0 to 4). The gradient is computed as the difference between
-    consecutive DataFrames and averaged to obtain a single measure of change.
-
-    Args:
-        data (tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]): A tuple containing
-            exactly three DataFrames representing successive states of a dataset.
-
-    Returns:
-        pd.DataFrame: A DataFrame containing the averaged gradient of change,
-        with the same structure as the input DataFrames.
-
-    Raises:
-        ValueError: If the input tuple does not contain exactly three DataFrames.
-
-    Notes:
-        - Since the input data represents Likert-scale responses (0 to 4), the gradient
-          values will always range from -2 to 2:
-            - The minimum possible gradient (-2) occurs when a response decreases by 2 points
-              in consecutive steps (e.g., from 4 → 2 → 0).
-            - The maximum possible gradient (2) occurs when a response increases by 2 points
-              in consecutive steps (e.g., from 0 → 2 → 4).
-            - A gradient of 0 indicates no overall change in the response trend.
-        - The gradient is computed as:
-          1. The difference between the second and first DataFrame (`data[1] - data[0]`).
-          2. The difference between the third and second DataFrame (`data[2] - data[1]`).
-          3. The final gradient is the mean of these two differences.
-          4. To simplify the mathematical expresion, the mean gradient is calculated as
-             `(data[2] - data[0]) / 2`.This is equivalent to the previous step-by-step
-        - This approach smooths the changes over time, reducing noise in individual step differences.
-
-    Example:
-        >>> df1 = pd.DataFrame({"A": [1, 2, 3], "B": [4, 3, 2]})
-        >>> df2 = pd.DataFrame({"A": [2, 3, 4], "B": [3, 2, 1]})
-        >>> df3 = pd.DataFrame({"A": [3, 4, 4], "B": [2, 1, 0]})
-        >>> calculate_gradient((df1, df2, df3))
-             A    B
-        0  1.0 -1.0
-        1  1.0 -1.0
-        2  0.5 -1.0
-
-    """
-    if len(data) != 3:
-        raise ValueError("The 'data' tuple must contain exactly three DataFrames.")
-
-    # calculate the gradient as the mean of the differences between successive DataFrames
-    mean_gradient = (data[2] - data[0]) / 2
-    return mean_gradient
 
 
 def likert_counts(
